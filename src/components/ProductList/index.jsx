9045b02f4c44999a7b659c11c741a2fa3853df94
TienLe <leviettien1808@gmail.com>
2022-01-01 00:14:21 +0700

    Create ProductList component.

---
+++ src/components/ProductList/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div>Hello, friend! I am a basic React component</div>
+        )
+    }
+}
+
+export default ProductList
