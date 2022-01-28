c0e3ccd6289bc01b94ad436942695e5a4e2aa09f
TienLe <leviettien1808@gmail.com>
2022-01-29 00:31:58 +0700

    Create static data.

---
+++ src/seed.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: 1,
+    title: "Yello Pail",
+    description: "On-demand sand castle construction expertise.",
+    url: "#",
+    votes: parseInt(Math.random() * 100),
+    submitterAvatarUrl: `https://i.pravatar.cc/40?img=${parseInt(Math.random() * 70)}`,
+    productImageUrl: "https://via.placeholder.com/150x150",
+  },
+  {
+    id: 2,
+    title: "Supermajority: The Fantasy Congress League",
+    description: "Earn points when your favorite politicians pass legislation.",
+    url: "#",
+    votes: parseInt(Math.random() * 100),
+    submitterAvatarUrl: `https://i.pravatar.cc/40?img=${parseInt(Math.random() * 70)}`,
+    productImageUrl: "https://via.placeholder.com/150x150",
+  },
+  {
+    id: 3,
+    title: "Tinfoild: Tailored tinfoil hats",
+    description: "We already have your measurements and shipping address.",
+    url: "#",
+    votes: parseInt(Math.random() * 100),
+    submitterAvatarUrl: `https://i.pravatar.cc/40?img=${parseInt(Math.random() * 70)}`,
+    productImageUrl: "https://via.placeholder.com/150x150",
+  },
+  {
+    id: 4,
+    title: "Haught or Naught",
+    description: "High-minded or absent-minded? You decide.",
+    url: "#",
+    votes: parseInt(Math.random() * 100),
+    submitterAvatarUrl: `https://i.pravatar.cc/40?img=${parseInt(Math.random() * 70)}`,
+    productImageUrl: "https://via.placeholder.com/150x150",
+  },
+];
+
+export default products;
