5bb5d5a805263504d7c98128caf23bda5b0eb68e
TienLe <leviettien1808@gmail.com>
2022-01-29 00:32:43 +0700

    Update ProductList to show Product.

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import Product from "../Product";
+import Seed from "../../seed";
 
 class ProductList extends React.Component {
-    render() {
-        return (
-            <div>Hello, friend! I am a basic React component</div>
-        )
-    }
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    this.setState({ products: Seed });
+  }
+
+  handleProductUpVote = (productId) => {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({ products: nextProducts });
+  };
+
+  render() {
+    const products = this.state.products.sort((a, b) => b.votes - a.votes);
+    const productComponents = products.map((product) => (
+      <Product
+        key={"product-" + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
+      />
+    ));
+
+    return <div>{productComponents}</div>;
+  }
 }
 
-export default ProductList
+export default ProductList;
